c02cde3f1c31293501afccd69634bb95813ffe83
Vasili Gulevich <vasili.gulevich@xored.com>
2026-01-08 00:03:47 +0400

    Extract rat images for training

--- rat_tracer/extract_keyframes.py
+++ rat_tracer/extract_keyframes.py
@@ -5,20 +5,22 @@ from xml.etree import ElementTree
 from typing import Dict, Generator, Tuple
 from imageio import imwrite
 from imageio_ffmpeg import read_frames
-from numpy import frombuffer, uint8
+from numpy import frombuffer, ndarray, uint8
 
 OUT_DIR = Path("rat_images")
 OUT_DIR.mkdir(exist_ok=True)
 
 
-def iter_frames(video_path, *, pix_fmt="rgb24"):
+def iter_frames(video_path, *, pix_fmt="rgb24") -> Generator[Tuple[int, ndarray]]:
     """
     Yield (frame_index, frame_array) for all decoded frames.
 
     frame_array shape: (height, width, 3), dtype=uint8
     """
     reader = read_frames(video_path, pix_fmt=pix_fmt)
-    meta = next(reader)
+    meta = next(reader, None)
+    if not meta:
+        return
     width, height = meta["size"]
 
     for idx, frame_bytes in enumerate(reader):
@@ -56,9 +58,8 @@ def extract_box_coordinates(xml_path: str) -> Generator[Tuple[int, float, float,
         except (TypeError, ValueError):
             # Skip boxes with missing or non-float attributes
             continue
-by_frame: Dict[int, Tuple[float, float, float, float]] = {}
 
-def extract_region(frame, x1:float, y1:float, x2:float, y2:float):
+def extract_region(frame:ndarray, x1:float, y1:float, x2:float, y2:float):
     """
     Extract a rectangular region from a frame.
 
@@ -69,13 +70,17 @@ def extract_region(frame, x1:float, y1:float, x2:float, y2:float):
     """
     return frame[int(y1):int(y2), int(x1):int(x2)]
 
-for i in extract_box_coordinates('input/annotations.xml'):
-    by_frame[i[0]] = i[1:]
+def main():
+    by_frame: Dict[int, Tuple[float, float, float, float]] = {}
+    for i in extract_box_coordinates('input/annotations.xml'):
+        by_frame[i[0]] = i[1:]
 
+    for idx, frame in iter_frames("input/input.mp4"):
+        if idx not in by_frame:
+            continue
+        region = by_frame.pop(idx)
+        frame = extract_region(frame, *region)
+        imwrite(OUT_DIR / f"frame_{idx}.png", frame)
 
-for idx, frame in iter_frames("input/input.mp4"):
-    if idx not in by_frame:
-        continue
-    region = by_frame.pop(idx)
-    frame = extract_region(frame, *region)
-    imwrite(OUT_DIR / f"frame_{idx}.png", frame)
+if __name__ == "__main__":
+    main()
