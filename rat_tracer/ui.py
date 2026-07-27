439f1fdb76aa6f17b9f4451baabc840b6580f5b7
Vasili Gulevich <vasili.gulevich@xored.com>
2026-07-27 23:40:11 +0400

    Do not show obsolete frame while loading preprocessed coverage #4

--- rat_tracer/ui.py
+++ rat_tracer/ui.py
@@ -158,6 +158,7 @@ class VideoMasker(QObject):
         self._position = 0.0
         self._cap = None
         self._total_frame_count = 0.0
+        self._video_sink.setVideoFrame(QVideoFrame())
 
     def _get_playing(self) -> bool:
         return self._playing
